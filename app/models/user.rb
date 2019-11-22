56c4e2589de4a8284f7e3d1b43a75d906a9d9d02
takeshi-shiihara <shi4490@icloud.com>
2019-11-23 06:13:12 +0900

    foodテーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          validates :name, :email, :password, :gender, presence: true
          enum gender: { draft: 0, published: 1 }
+
+  has_many :foods
 end
---
+++ db/migrate/20191122210919_create_foods.rb
@@ -0,0 +1,7 @@
+class CreateFoods < ActiveRecord::Migration[5.2]
+  def change
+    create_table :foods do |t|
+      t.timestamps
+    end
+  end
+end
