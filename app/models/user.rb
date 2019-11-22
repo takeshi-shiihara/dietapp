f4b4ab903e31e70d139fef0aad5681d112415efd
takeshi-shiihara <shi4490@icloud.com>
2019-11-22 22:20:33 +0900

    メイン画面　＋　db enum(性別カラム)作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates :name, :email, :password, :gender, presence: true
+         enum gender: { draft: 0, published: 1 }
 end
