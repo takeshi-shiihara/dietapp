8ce0a91ea4acff813d20cd6436e9a2e447f57ad8
takeshi-shiihara <shi4490@icloud.com>
2019-11-23 18:25:02 +0900

    foodsコントローラ作成

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,9 @@
+class FoodsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'main#index'
   resources :users, only: [:edit, :update, :show, :new, :create]
+  resources :foods, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
