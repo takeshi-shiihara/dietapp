4537fc8b3e51572a946c728a743ae9f70994b7a2
takeshi-shiihara <shi4490@icloud.com>
2019-12-04 12:47:42 +0900

    レコード機能の実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,31 @@
 class FoodsController < ApplicationController
+  before_action :set_user
 
   def new
     if user_signed_in?
+      @foods = Food.new
     else
       redirect_to root_path
     end
   end
 
   def create
+    @foods = Food.new(foods_params)
+    if @foods.save
+      redirect_to root_path
+    else
+      redirect_to controller: :foods, action: :new
+    end
+  end
+
+  private
+
+  def foods_params
+    params.require(:food).permit(:carbonhydrate, :protein, :lipid).merge(user_id: current_user.id)
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
   end
 
 end
