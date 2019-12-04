99ba9de2fcfb168cc86195d1f6a096eaaf9a7576
takeshi-shiihara <shi4490@icloud.com>
2019-12-04 19:47:19 +0900

    foodsの値が空の時の処理

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,11 +1,20 @@
 class MainController < ApplicationController
+  before_action :set_user
 
   def index
     if user_signed_in?
-      @food = Food.find(current_user.id)
-      @data = [["炭水化物", @food.carbonhydrate], ["タンパク質", @food.protein], ["脂質", @food.lipid]]
-      @weight = Weight.weight.each
+      if @user.foods.present?
+        @food = Food.find(current_user.id)
+        @data = [['炭水化物', @food.carbonhydrate], ['タンパク質', @food.protein], ['脂質', @food.lipid]]
+      else
+      end
     end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
   end
   
 end
