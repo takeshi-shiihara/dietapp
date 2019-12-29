425c8488cc974faa8503cdbf03e6408023989944
takeshi-shiihara <shi4490@icloud.com>
2019-12-30 05:56:55 +0900

    fontawesome、ヘッダー

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'main#index'
   resources :users, only: [:edit, :update, :show, :new, :create]
   resources :foods, only: [:new, :create]
+  resources :weights, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
