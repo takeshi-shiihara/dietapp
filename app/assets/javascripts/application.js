9be1399e5e52cbe4c0b63d5353f56f2e6845522f
takeshi-shiihara <shi4490@icloud.com>
2020-01-03 17:02:37 +0900

    ヘッダー(ドロップダウンメニュー)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 
-//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require activestorage
 //= require_tree .
 //= require chartkick
--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,5 +1,16 @@
 $(function() {
-  $(".post").hover(function() {
-    $(this)
-  });
+
+  var html = `<div class="dropdwn">
+                <a href="/foods/new">食べ物</a>
+              </div>`
+
+
+  $(".post").hover(
+    function() {
+      $(this).append(html);
+    },
+    function() {
+      $(".dropdwn").remove();
+    }
+  );
 });
