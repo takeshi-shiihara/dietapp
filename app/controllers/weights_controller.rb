9eb5f7e40abfb7c1a036f850ff4f714df27a2722
takeshi-shiihara <shi4490@icloud.com>
2020-01-09 19:27:20 +0900

    weightフォーム

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -8,4 +8,20 @@ class WeightsController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def create
+    @weights = Weight.new(weights_params)
+    @date = Date.today
+    if @weights.save
+      redirect_to root_path
+    else
+      redirect_to controller: :weights, action: :new
+    end
+  end
+
+  private
+
+  def weights_params
+    params.reqire(:weight).permit(:weight).merge(user_id: current_user.id)
+  end
 end
