de0149fc6b18f111f902583f187e682adbdb7cb2
takeshi-shiihara <shi4490@icloud.com>
2020-01-09 14:51:47 +0900

    weight.new作成

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -2,7 +2,7 @@ $(function() {
 
   var html = `<ul class="dropdwn">
                 <li><a href="/foods/new" class="record-link">食べ物を記録する</a></li>
-                <li class="weight"><a href="" class="record-link">体重を記録する</a></li>
+                <li class="weight"><a href="/weights/new" class="record-link">体重を記録する</a></li>
               </ul>`
 
 
--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -1,2 +1,6 @@
 class WeightsController < ApplicationController
+
+  def new
+    @weight = Weight.new
+  end
 end
