b19c9075bd25cab6080e450ca9123b3234bbe89c
takeshi-shiihara <shi4490@icloud.com>
2020-01-09 16:17:50 +0900

    weight.newコントローラー

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -1,6 +1,11 @@
 class WeightsController < ApplicationController
 
   def new
-    @weight = Weight.new
+    if user_signed_in?
+      @weight = Weight.new
+      @date = Date.today  
+    else
+      redirect_to root_path
+    end
   end
 end
